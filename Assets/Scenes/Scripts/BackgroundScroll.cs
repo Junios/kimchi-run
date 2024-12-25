145cf6ff2cc616c7c05725b71af3aed082049547
junios <junios@junios.net>
2024-12-25 19:41:31 +0900

    init

---
+++ Assets/Scenes/Scripts/BackgroundScroll.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+public class BackgroundRolling : MonoBehaviour
+{
+    [Header("Settings")]
+    [Tooltip("How fast should the texture scroll?")]
+    public float scrollSpeed;
+
+    [Header("References")]
+    public MeshRenderer meshRenderer;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        meshRenderer = GetComponent<MeshRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        meshRenderer.material.mainTextureOffset += new Vector2(scrollSpeed, 0) * Time.deltaTime;
+    }
+}
